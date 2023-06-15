5228f75a92167ab9366f4d6ed85ce8a1c25af8b8
Itay Bardan <itay.bardan@gmail.com>
2023-06-15 11:21:36 +0300

    add test skeleton

---
+++ __tests__/example.ts
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+
+
+describe("example", () => {
+    it("example", () => {
+        // check if all components are rendered
+        expect(true).toBe(true);
+    });
+});
